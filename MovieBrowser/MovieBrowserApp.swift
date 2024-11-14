5d0720dae947f2a955cf76a59864b57fe5e4ebbb
Your Name <you@example.com>
2024-11-13 20:30:56 -0600

    Initial Commit

---
+++ MovieBrowser/MovieBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBrowserApp.swift
+//  MovieBrowser
+//
+//  Created by Etay Luz on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
